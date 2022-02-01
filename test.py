5d91c9a8c61211e6e0aa995c31d458d5627ae630
jeffreyjgong <jeffrey.gong7@gmail.com>
2022-01-31 20:58:51 -0500

    Add requirements for pip

--- test.py
+++ test.py
@@ -1,18 +1,22 @@
 import requests 
 import lxml.html as lxml
-import pandas as pd
 import re
 
+# First, get url of the data
 url='https://questionnaire-148920.appspot.com/swe/data.html'
 
+# Perform GET request to get html
 page_html = requests.get(url)
 
+# Convert into a doc using lxml
 contents = lxml.fromstring(page_html.content)
 
+# Parse for table row elements
 table_rows = contents.xpath('//tr')
-
-count = 0
 print('Total rows of salary data: ', len(table_rows))
+
+# Write correct and malformed outputs to separate text files
+count = 0
 
 with open('test_outputs\malformed_outputs.txt', 'w') as f_1, open('test_outputs\correct_outputs.txt', 'w') as f_2:
    for t in table_rows:
@@ -25,5 +29,3 @@ with open('test_outputs\malformed_outputs.txt', 'w') as f_1, open('test_outputs\
          f_2.write(x.group() + '\n')
 
 print('Total malformed: ', count)
-
-
